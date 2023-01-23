56e7ea5d87c64b91489c81069cfaf208715d9bf5
Marian Hahne <marian@marianhahne.de>
2023-01-23 16:57:06 +0100

    App/Server: Init express

--- apps/server/index.ts
+++ apps/server/index.ts
@@ -1,1 +1,10 @@
+import express from "express";
+
 console.log("Hello World");
+
+// init and start express server
+const app = express();
+
+app.listen(3000, () => {
+    console.log("Server started on port 3000");
+});
