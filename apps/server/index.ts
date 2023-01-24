80bbb4d46ba6460293b375bd4c5f5f76733d8f1b
Marian Hahne <marian@marianhahne.de>
2023-01-24 17:49:18 +0100

    Add GraphQL endpoint

--- apps/server/index.ts
+++ apps/server/index.ts
@@ -1,10 +1,37 @@
 import express from "express";
+import { buildSchema } from "graphql";
+import { graphqlHTTP } from "express-graphql";
 
 console.log("Hello World");
 
 // init and start express server
 const app = express();
 
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+    type Query {
+        hello: String
+    }
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+    hello: () => {
+        return "Hello world!";
+    },
+};
+
+// add graphql endpoint
+app.use(
+    "/graphql",
+    graphqlHTTP({
+        schema: schema,
+        rootValue: root,
+        graphiql: true,
+    }),
+);
+
 app.listen(3000, () => {
-    console.log("Server started on port 3000");
+    console.log("Server started...");
+    console.log("GraphQL-Endpoint: http://localhost:3000/graphql");
 });
